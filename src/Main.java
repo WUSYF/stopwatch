f4b1ca1745e91afe648529d197299d641596d65a
WUSYF <daniel.hofma@gmail.com>
2019-03-02 15:01:36 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
 
+public class name extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
     }
 }
